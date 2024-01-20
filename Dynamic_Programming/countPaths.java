41c93b4dd783dde7105790abe02ca44a2a17391a
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-01-20 16:39:26 -0500

    working on countPaths

--- Dynamic_Programming/countPaths.java
+++ Dynamic_Programming/countPaths.java
@@ -1,8 +1,27 @@
 package Dynamic_Programming;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class countPaths {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-        
+
+    public static int paths(List<List<String>> grid) {
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        int count = 1;
+        for (int row = 0; row < grid.size(); ++row) {
+            for (int col = 0; col < grid.get(0).size(); ++col) {
+                if (check(row, col, grid, memo)) {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static boolean check(int row, int col, List<List<String>> grid, HashMap<Integer, Integer> memo) {
+        return false;
+    }
 }
