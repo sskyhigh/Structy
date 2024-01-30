65ef5a3e1f38ecd85f644c7060ead32d8d76fc44
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-01-30 13:36:24 -0500

    nonAdjacentSum completed

---
+++ Dynamic_Programming/nonAdjacentSum.java
@@ -0,0 +1,30 @@
+package Dynamic_Programming;
+
+import java.util.*;
+
+public class nonAdjacentSum {
+    public static void main(String[] args) {
+        List<Integer> nums = List.of(2, 4, 5, 12, 7);
+        System.out.println(nonAdjacentSum(nums));
+    }
+
+    public static int nonAdjacentSum(List<Integer> nums) {
+        return nonAdjacentSum(nums, 0, new HashMap<>());
+    }
+
+    public static int nonAdjacentSum(List<Integer> nums, int i, HashMap<Integer, Integer> memo) {
+        // out of bounds
+        if(i >= nums.size()){
+            return 0; 
+        }
+        if(memo.containsKey(i)){
+            return memo.get(i);
+        }
+        int result = Math.max(
+        nums.get(i) + nonAdjacentSum(nums, i+2, memo),
+        nonAdjacentSum(nums, i+1, memo)
+        );
+        memo.put(i, result);
+        return result;
+    }
+}
