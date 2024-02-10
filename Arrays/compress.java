92b6f25ccfc8ada1024aab6045cc9fa09532935a
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-10 15:50:21 -0500

    compress

---
+++ Arrays/compress.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class compress {
+    public static void main(String[] args) {
+        //System.out.println("Hello world");
+        System.out.println(compress("ccaaatssss")); // print 2c3at4s 
+    }
+
+    public static String compress(String s) {
+        //String letters = "abcdefghijklmnopqrstuvwxyz";
+        String result = "";
+        int i = 0, j = 0;
+        while (j < s.length()) {
+            //String characters = String.valueOf(s.charAt(j));
+            if (s.charAt(i)==s.charAt(j)) {
+                ++j;
+            } else {
+                // if there is one character, just print the character
+                int number = j - i;
+                if(number > 1){
+                    result += "" + number + s.charAt(i);
+                }
+                // gives us sum
+                else{
+                    result += "" + s.charAt(i);
+                }
+                // move the index
+                i = j;
+            }
+        }
+        return result;
+    }
+}
