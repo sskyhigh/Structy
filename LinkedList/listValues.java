5ad804ba62551565b3ead796dd0bbe902cb338cf
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-11 17:03:58 -0500

    changes

--- LinkedList/listValues.java
+++ LinkedList/listValues.java
@@ -15,6 +15,23 @@ class Node<T> {
 public class listValues {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Node<String> a = new Node<>("a");
+        Node<String> b = new Node<>("b");
+        Node<String> c = new Node<>("c");
+        Node<String> d = new Node<>("d");
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        System.out.println(linkedListValues(a));
     }
 
+    public static List<String> linkedListValues(Node<String> head) {
+        List<String> result = new ArrayList<>();
+        Node<String> current = head;
+        while (current != null) {
+            result.add(current.val);
+            current = current.next;
+        }
+        return result;
+    }
 }
