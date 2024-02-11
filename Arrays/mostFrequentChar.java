891e4fee0a3615df8a74d171329a4bd215b083c6
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-10 21:27:02 -0500

    finished mostFrequent

--- Arrays/mostFrequentChar.java
+++ Arrays/mostFrequentChar.java
@@ -1,17 +1,21 @@
 package Arrays;
-import java.util.*; 
+
+import java.util.*;
 
 public class mostFrequentChar {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    public static char mostFrequentChar(String s){
-        HashMap<Character, Integer> count = new HashMap<>(); 
-        for(char c: s.toCharArray()){
+
+    public static char mostFrequentChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<>();
+        for (char c : s.toCharArray()) {
             // checking if doesn't exist
-            if(count.get(c)==null){
+            if (count.get(c) == null) {
                 count.put(c, 0);
             }
+            count.put(c, count.get(c) + 1);
         }
+        return 'c';
     }
 }
