a1effc516557ac89621ed994f194f3963ba50de6
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-11 17:00:28 -0500

    listValues

--- LinkedList/listValues.java
+++ LinkedList/listValues.java
@@ -1,9 +1,20 @@
 package LinkedList;
+
 import java.util.*;
+
+class Node<T> {
+    T val;
+    Node<T> next;
+
+    public Node(T val) {
+        this.val = val;
+        this.next = null;
+    }
+}
 
 public class listValues {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
-    
+
 }
