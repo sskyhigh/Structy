fb1a75946fff0b072497568755d5372eb432dc75
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-12 20:38:02 -0500

    reverse some nodes

---
+++ LinkedList/reverseList.java
@@ -0,0 +1,11 @@
+package LinkedList;
+import java.util.*; 
+
+public class reverseList {
+    public static void main(String[] args) {
+        
+    }
+    public static <T> Node <T> reverseList(Node<T>head){
+        return head; 
+    }
+}
