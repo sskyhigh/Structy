af6246debb09c6fa3fb0d9190eba38627c685206
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-14 22:11:57 -0500

    made changes

---
+++ LinkedList/mergeList.java
@@ -0,0 +1,10 @@
+package LinkedList;
+
+public class mergeList {
+    public static void main(String[] args) {
+        
+    }
+    public static Node<Integer> mergeLists(Node<Integer> head1, Node<Integer> head2){
+        return null; 
+    }
+}
