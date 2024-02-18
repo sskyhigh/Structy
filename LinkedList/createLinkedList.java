ff312048bf2edf8df0c491addec944e3346db034
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-18 18:45:50 -0500

    finished createLinkedList

---
+++ LinkedList/createLinkedList.java
@@ -0,0 +1,28 @@
+package LinkedList;
+import java.util.*; 
+
+public class createLinkedList {
+    public static void main(String[] args) {
+        //System.out.println("hello world");
+        printList(createLinkedList(List.of("h","e","y")));
+    }
+    public static <T> Node<T> createLinkedList(List<T> values){
+        return createLinkedList(values, 0);
+    }
+
+    public static <T> Node<T> createLinkedList(List<T> values, int index){
+        if(index >= values.size()){
+            return null; 
+        }
+        Node<T> newNode = new Node<>(values.get(index));
+        newNode.next = createLinkedList(values, index+1);
+        return newNode; 
+    }
+    public static <T> void printList(Node<T> head){
+		Node<T> temp = head; 
+		while(temp!=null){
+			System.out.print(temp + " ");
+			temp = temp.next; 
+		}
+	}
+}
