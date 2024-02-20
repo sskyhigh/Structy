2594e4aec45ee9643ad45fefd7f77661815b8322
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-19 21:33:37 -0500

    made changes

---
+++ LinkedList/addLists.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+public class addLists {
+    public static void main(String[] args) {
+
+    }
+
+    public static Node<Integer> addLists(Node<Integer> head1, Node<Integer> head2) {
+        
+        return null;
+    }
+}
