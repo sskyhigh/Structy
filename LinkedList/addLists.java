2d280177b746ed09d4713529b70c8de7ac4257d5
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-20 23:32:12 -0500

    worked on addlists.java

--- LinkedList/addLists.java
+++ LinkedList/addLists.java
@@ -3,27 +3,37 @@ package LinkedList;
 import javax.xml.transform.Source;
 
 public class addLists {
+    // solving recursively
     public static void main(String[] args) {
-        Node<Integer> a1 = new Node<>(1);
-        Node<Integer> a2 = new Node<>(2);
-        Node<Integer> a3 = new Node<>(6);
+        Node<Integer> a1 = new Node<>(9);
+        Node<Integer> a2 = new Node<>(8);
         a1.next = a2;
-        a2.next = a3;
-        // 1 -> 2 -> 6
+        // 9 -> 8
 
-        Node<Integer> b1 = new Node<>(4);
-        Node<Integer> b2 = new Node<>(5);
-        Node<Integer> b3 = new Node<>(3);
+        Node<Integer> b1 = new Node<>(7);
+        Node<Integer> b2 = new Node<>(4);
         b1.next = b2;
-        b2.next = b3;
-        // 4 -> 5 -> 3 
-
-        addLists(a1, b1);
-        // 5 -> 7 -> 9
+        // 7 -> 4
+        System.out.println(addLists(a1, b1));
     }
 
     public static Node<Integer> addLists(Node<Integer> head1, Node<Integer> head2) {
+        return addLists(head1, head2, 0);
+    }
 
-        return null;
+    public static Node<Integer> addLists(Node<Integer> head1, Node<Integer> head2, int carry) {
+        if (head1 == null && head2 == null && carry == 0) {
+            return null;
+        }
+        int value1 = head1 == null ? 0 : head1.val;
+        int value2 = head1 == null ? 0 : head2.val;
+        int sum = value1 + value2 + carry;
+        int nextCarry = sum >= 10 ? 1 : 0;
+        int digit = sum % 10;
+        Node<Integer> newNode = new Node<>(digit);
+        Node<Integer> next1 = head1 == null ? null : head1.next;
+        Node<Integer> next2 = head2 == null ? null : head2.next;
+        newNode.next = addLists(next1, next2, nextCarry);
+        return newNode;
     }
 }
