a36aa3cdf1244492009be89c3c9b26910e45b39e
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-20 21:27:20 -0500

    made changes

--- LinkedList/addLists.java
+++ LinkedList/addLists.java
@@ -1,12 +1,29 @@
 package LinkedList;
 
+import javax.xml.transform.Source;
+
 public class addLists {
     public static void main(String[] args) {
+        Node<Integer> a1 = new Node<>(1);
+        Node<Integer> a2 = new Node<>(2);
+        Node<Integer> a3 = new Node<>(6);
+        a1.next = a2;
+        a2.next = a3;
+        // 1 -> 2 -> 6
 
+        Node<Integer> b1 = new Node<>(4);
+        Node<Integer> b2 = new Node<>(5);
+        Node<Integer> b3 = new Node<>(3);
+        b1.next = b2;
+        b2.next = b3;
+        // 4 -> 5 -> 3 
+
+        addLists(a1, b1);
+        // 5 -> 7 -> 9
     }
 
     public static Node<Integer> addLists(Node<Integer> head1, Node<Integer> head2) {
-        
+
         return null;
     }
 }
