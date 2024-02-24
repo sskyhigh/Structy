9adf86a2b13dffad2c65d62bfabf950df64cf89a
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-24 13:25:15 -0500

    made small changes to addLists

--- LinkedList/addLists.java
+++ LinkedList/addLists.java
@@ -7,7 +7,7 @@ public class addLists {
     public static void main(String[] args) {
         Node<Integer> a1 = new Node<>(9);
         Node<Integer> a2 = new Node<>(8);
-        a1.next = a2;  
+        a1.next = a2;
         // 9 -> 8
 
         Node<Integer> b1 = new Node<>(7);
@@ -22,25 +22,22 @@ public class addLists {
     }
 
     public static Node<Integer> addLists(Node<Integer> head1, Node<Integer> head2, int carry) {
-        if (head1 == null && head2 == null && carry == 0) {
+        if (head1 == null && head2 == null) {
             return null;
         }
         int value1 = head1 == null ? 0 : head1.val;
-        int value2 = head1 == null ? 0 : head2.val;
-        int sum = value1 + value2 + carry;
-        int nextCarry = sum >= 10 ? 1 : 0;
-        int digit = sum % 10;
-        Node<Integer> newNode = new Node<>(digit);
-        Node<Integer> next1 = head1 == null ? null : head1.next;
-        Node<Integer> next2 = head2 == null ? null : head2.next;
-        newNode.next = addLists(next1, next2, nextCarry);
+        int value2 = head2 == null ? 0 : head2.val;
+        int sum = value1 + value2;
+        Node<Integer> newNode = new Node<>(sum);
+        newNode.next = addLists(head1.next, head2.next, carry);
         return newNode;
     }
-    public static <T> void printList(Node<T> head){
-		Node<T> temp = head; 
-		while(temp!=null){
-			System.out.print(temp + " ");
-			temp = temp.next;  
-		}
-	}
+
+    public static <T> void printList(Node<T> head) {
+        Node<T> temp = head;
+        while (temp != null) {
+            System.out.print(temp + " ");
+            temp = temp.next;
+        }
+    }
 }
