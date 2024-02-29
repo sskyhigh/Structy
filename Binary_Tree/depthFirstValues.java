14ddec2c039284a4f8a404376ef2e44eacc7ac55
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-02-29 12:54:24 -0500

    started depthfirstvalues

--- Binary_Tree/depthFirstValues.java
+++ Binary_Tree/depthFirstValues.java
@@ -1,7 +1,27 @@
 package Binary_Tree;
 
+import java.util.*;
+
+// recursive method. 
+class Node<T> {
+    T val;
+    Node<T> left;
+    Node<T> right;
+
+    public Node(T val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
 public class depthFirstValues {
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    public static List<String> depthFirstValues(Node<String> root) {
+        
+        return List.of();
     }
 }
