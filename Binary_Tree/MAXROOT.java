03e056826c3cb572924c163c3e36d434794c0057
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-03-04 14:43:56 -0500

    comments

--- Binary_Tree/MAXROOT.java
+++ Binary_Tree/MAXROOT.java
@@ -1,16 +1,33 @@
+// O(m) time ans space
 package Binary_Tree;
-import java.util.*; 
+
+import java.util.*;
 
 public class MAXROOT {
     public static void main(String[] args) {
-        
+        Node<Double> a = new Node<>(3.);
+        Node<Double> b = new Node<>(11.);
+        Node<Double> c = new Node<>(4.);
+        Node<Double> d = new Node<>(4.);
+        Node<Double> e = new Node<>(-2.);
+        Node<Double> f = new Node<>(1.);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        c.right = f;
+
+        System.out.println(maxPathSum(a));
     }
-    public static Double maxPathSum(Node<Double>root){
-        if(root==null){
+
+    public static Double maxPathSum(Node<Double> root) {
+        if (root == null) {
             return Double.NEGATIVE_INFINITY;
         }
-        // leaf has no children. 
-        if(root.left==null&&root.right==null) return root.val; 
-
+        // leaf has no children.
+        if (root.left == null && root.right == null)
+            return root.val;
+        return root.val + Math.max(maxPathSum(root.left), maxPathSum(root.right));
     }
 }
