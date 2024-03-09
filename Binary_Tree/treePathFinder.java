8c03612c34eb3151aaeb5aa70067bebe17d66d54
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-03-09 15:20:15 -0500

    started pathFinder

---
+++ Binary_Tree/treePathFinder.java
@@ -0,0 +1,15 @@
+package Binary_Tree;
+import java.util.*; 
+
+public class treePathFinder {
+    public static void main(String[] args) {
+        
+    }
+    public static <T> List<T> pathFinder(Node<T> root, T target){
+        List<T> list = new ArrayList<>(); 
+        if(root==null){
+            return null; 
+        }
+        return list;
+    }
+}
