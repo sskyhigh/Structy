cac39edc7b327d27d33f7a1afba3331d4f0d20b3
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-03-11 12:32:39 -0400

    finished pathFinder problem

--- Binary_Tree/treePathFinder.java
+++ Binary_Tree/treePathFinder.java
@@ -3,12 +3,42 @@ import java.util.*;
 
 public class treePathFinder {
     public static void main(String[] args) {
-        
+        Node<String> a = new Node<>("a");
+		Node<String> b = new Node<>("b");
+		Node<String> c = new Node<>("c");
+		Node<String> d = new Node<>("d");
+		Node<String> e = new Node<>("e");
+		Node<String> f = new Node<>("f");
+
+		a.left = b;
+		a.right = c;
+		b.left = d;
+		b.right = e;
+		c.right = f;
+
+        System.out.println(pathFinder(a, "e"));
     }
-    public static <T> List<T> pathFinder(Node<T> root, T target){
-        List<T> list = new ArrayList<>(); 
-        if(root==null){
-            return null; 
+
+    public static <T> List <T> pathFinder(Node<T> root, T target){
+        List<T> path = pathFinderRecursive(root, target);
+        if(path==null) return null;
+        else Collections.reverse(path);
+        return path; 
+    }
+
+    public static <T> List<T> pathFinderRecursive(Node<T> root, T target){
+        if(root==null) return null; 
+        if(root.val == target){
+            List<T> list = new ArrayList<>(); 
+            list.add(root.val); return list; 
+        }
+        List<T> leftSide = pathFinderRecursive(root.left, target);
+        if(leftSide!=null){
+            leftSide.add(root.val); return leftSide; 
+        }
+        List<T> rightSide = pathFinderRecursive(root.right, target);
+        if(rightSide!=null){
+            rightSide.add(root.val); return rightSide; 
         }
         return null;
     }
