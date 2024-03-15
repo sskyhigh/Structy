9006c121b7c424ed46e65a7a8f5cb411045d435b
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-03-14 23:26:29 -0400

    started bottomRightValue

---
+++ Binary_Tree/bottomRightValue.java
@@ -0,0 +1,17 @@
+package Binary_Tree;
+import java.util.*; 
+
+public class bottomRightValue {
+    public static void main(String[] args) {
+        
+    }
+    public static <T> T bottomRightValoue(Node<T> root){
+        Queue<Node<T>> queue = new ArrayDeque<>(); 
+        queue.add(root);
+        Node<T> node = null; 
+        while(!queue.isEmpty()){
+            
+        }
+        return node.val; 
+    }
+}
