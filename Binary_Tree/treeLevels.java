4acab01ed230d58568fc674013a9b40274c1a657
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-03-18 15:26:02 -0400

    strted treeLevels

---
+++ Binary_Tree/treeLevels.java
@@ -0,0 +1,12 @@
+package Binary_Tree;
+import java.util.*; 
+
+public class treeLevels {
+    public static void main(String[] args) {
+        
+    }
+    public static <T> List<List<T>> treeLevels(Node<T> root){
+        List<List<T>> levels = new ArrayList<>(); 
+        return levels; 
+    }
+}
