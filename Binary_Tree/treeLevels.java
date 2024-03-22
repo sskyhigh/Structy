aaeb6d07be56ac3013830b7cd1dc47fd5e9253e4
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2024-03-21 20:29:21 -0400

    made changes

--- Binary_Tree/treeLevels.java
+++ Binary_Tree/treeLevels.java
@@ -7,6 +7,12 @@ public class treeLevels {
     }
     public static <T> List<List<T>> treeLevels(Node<T> root){
         List<List<T>> levels = new ArrayList<>(); 
+        treeLevels(root, 0, levels);
         return levels; 
+    }
+    public static <T> void treeLevels(Node<T> root, int levelIndex, List<List<T>> levels){
+        if(root==null) return; 
+        treeLevels(root.left, levelIndex+1, levels);
+        treeLevels(root.right, levelIndex+1, levels);
     }
 }
